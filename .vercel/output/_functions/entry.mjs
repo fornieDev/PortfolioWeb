3de80c19ad4dc1c9fb64ab3f028722b1ba0a3ab7
Javier Fornié Ortega <jfornieortega@gmail.com>
2025-10-05 12:29:59 +0200

    Restaurando astroconfig

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_D5E6w1Gj.mjs';
-import { manifest } from './manifest_Bk8CyMvH.mjs';
+import { manifest } from './manifest_IkqK32lX.mjs';
 
 const serverIslandMap = new Map();;
 
@@ -35,7 +35,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "dca983cb-1570-4f2e-8984-984ed81316aa",
+    "middlewareSecret": "d0d56ac8-0142-44b9-80ce-de9719f8083b",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
